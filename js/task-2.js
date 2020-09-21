3e09b0b2fc7761fe5af63113a76406a37503d9da
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2020-09-22 01:48:17 +0300

    решение

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,10 @@
+// Напиши скрипт, который для каждого элемента массива`ingredients` создаст
+// отдельный`li`, после чего вставит все`li` за одну операцию в список
+//     `ul.ingredients`.Для создания DOM - узлов используй`document.createElement()`.
+
+// HTML
+//<ul id="ingredients" class="list"></ul>
+
 //  массив строк
 const ingredients = [
   'Картошка',
@@ -7,3 +14,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+// решение
+
+const listEl = document.querySelector('#ingredients');
+
+const element = ingredients.map(element => {
+  const item = document.createElement('li');
+  item.textContent = element;
+  return item;
+});
+
+listEl.append(...element);
