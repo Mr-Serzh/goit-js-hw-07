30c95755d3a40b01b059424d9c0cb3b9fb80e1ab
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2020-09-23 23:57:40 +0300

    решение

--- js/task-7.js
+++ js/task-7.js
@@ -6,3 +6,13 @@
 // <input id="font-size-control" type="range"/>
 // <br/>
 // <span id="text">Абракадабра!</span>
+
+const resizeEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+resizeEl.oninput = function () {
+  textEl.setAttribute(
+    'style',
+    `font-size: ${resizeEl.value / 4}px; color: #e5e110;`,
+  );
+};
