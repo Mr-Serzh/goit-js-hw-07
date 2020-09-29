55942f350ea393f0da8ec1eb45a7861a5eb7c43d
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2020-09-29 20:36:01 +0300

    new img

--- js/task-3.js
+++ js/task-3.js
@@ -24,8 +24,8 @@ const images = [
   },
   {
     url:
-      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+      'https://images.pexels.com/photos/3779762/pexels-photo-3779762.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    alt: 'Girl',
   },
 ];
 
