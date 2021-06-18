ccdc19f282c4f4c01be07d888f4bc4eec1c5ebd4
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2021-06-19 02:23:25 +0300

    Update task-8.js

--- js/task-8.js
+++ js/task-8.js
@@ -13,3 +13,43 @@
 // - Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 
 // Создай функцию`destroyBoxes()`, которая очищает`div#boxes`.
+
+const btnRender = document.querySelector(
+  "#controls button[data-action='render']",
+);
+const btnDestroy = document.querySelector(
+  "#controls button[data-action='destroy']",
+);
+const count = document.querySelector('#controls input');
+const boxes = document.querySelector('#boxes');
+
+btnRender.addEventListener('click', function () {
+  createBoxes(count.value);
+});
+btnDestroy.addEventListener('click', destroyBoxes);
+
+const randomColor = () => Math.floor(Math.random() * 255);
+
+function createBoxes(amount) {
+  if (+amount <= 0 || amount === '' || +amount > 100) {
+    boxes.insertAdjacentHTML(
+      'afterbegin',
+      '<p>Please choose amount of boxes you want to create (from 1 to 100)</p>',
+    );
+  } else {
+    destroyBoxes();
+    const tempArr = [];
+    for (let i = 0; i < +amount; i += 1) {
+      tempArr.push(document.createElement('div'));
+      tempArr[i].style.cssText = `width:${30 + i * 10}px; height:${
+        30 + i * 10
+      }px;
+		background-color:rgb(${randomColor()},${randomColor()},${randomColor()})`;
+    }
+    boxes.append(...tempArr);
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
